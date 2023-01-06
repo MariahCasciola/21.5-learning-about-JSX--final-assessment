7b4890249fd9b8df181b43c45ba067e9909c5553
MariahCasciola <117303840+MariahCasciola@users.noreply.github.com>
2023-01-06 17:19:52 -0600

    all tests passing

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ const shirts = 4;
 const pants = 6;
 const website = "https://www.thinkful.com/";
 
-const element = null;
+const element = (
+  <main>
+    <p className="paragraph">
+      Purchased {shirts} shirts and {pants} pants for a total of{" "}
+      {shirts + pants} items
+    </p>
+    <a href={website}>Website</a>
+  </main>
+);
 
-function App(){
-
+function App() {
+  return element;
 }
 
 export default App;
